e50757afe655842c834e5f0eb4da36282db1f86c
Ivan Subotic <400790+subotic@users.noreply.github.com>
2020-11-25 15:21:49 +0100

    clean out repo

--- node_modules/@knora/viewer/lib/property/list-value/list-value.component.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReadListValue } from '@knora/core';
-export declare class ListValueComponent {
-    valueObject: ReadListValue;
-    private _listValueObj;
-    constructor();
-}
--- node_modules/@knora/viewer/lib/property/uri-value/uri-value.component.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReadUriValue } from '@knora/core';
-export declare class UriValueComponent {
-    valueObject: ReadUriValue;
-    private __uriValueObj;
-    constructor();
-}
